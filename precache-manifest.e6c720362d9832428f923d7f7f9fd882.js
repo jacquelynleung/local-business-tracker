6d107cc14745d78f61d44bb379ebb497c54d1c75
JacquelynLeung <jil4009@g.rit.edu>
2020-11-05 16:32:39 -0500

    Updates

---
+++ precache-manifest.e6c720362d9832428f923d7f7f9fd882.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fbd540e046eacac76c5676068834acbf",
+    "url": "/local-business-tracker/index.html"
+  },
+  {
+    "revision": "3f8065969dd564e516dd",
+    "url": "/local-business-tracker/static/css/main.3c51cdd2.chunk.css"
+  },
+  {
+    "revision": "02a5484acdde3e078fc0",
+    "url": "/local-business-tracker/static/js/2.2182a150.chunk.js"
+  },
+  {
+    "revision": "9b318b6fb13190fe82c0677e9264b3c7",
+    "url": "/local-business-tracker/static/js/2.2182a150.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3f8065969dd564e516dd",
+    "url": "/local-business-tracker/static/js/main.9ed5e25c.chunk.js"
+  },
+  {
+    "revision": "9d637284d620390db0be",
+    "url": "/local-business-tracker/static/js/runtime-main.c0d65440.js"
+  },
+  {
+    "revision": "79afa8a76ca3077b3624a328fd6fa3ee",
+    "url": "/local-business-tracker/static/media/galeslogo.79afa8a7.png"
+  },
+  {
+    "revision": "083bb09d2b77fdf6f27fdbb7cd88ab6b",
+    "url": "/local-business-tracker/static/media/rating2.083bb09d.png"
+  },
+  {
+    "revision": "43a6b37e332b451004cc2973833c88a2",
+    "url": "/local-business-tracker/static/media/rating3.43a6b37e.png"
+  },
+  {
+    "revision": "20f639ae420873f677d19db78f342dae",
+    "url": "/local-business-tracker/static/media/rating5.20f639ae.png"
+  },
+  {
+    "revision": "0ae863c04dc2ba3815ddd48cb2f62899",
+    "url": "/local-business-tracker/static/media/smillinglogo.0ae863c0.png"
+  },
+  {
+    "revision": "be9189cc52e9c1b0faa3e92d9698a72a",
+    "url": "/local-business-tracker/static/media/sundaeslogo.be9189cc.png"
+  },
+  {
+    "revision": "b28faa371d877306b39df351095bd147",
+    "url": "/local-business-tracker/static/media/user123.b28faa37.png"
+  }
+]);
